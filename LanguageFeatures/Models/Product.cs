72f17111f769ebc5b851945c96ec3ecc00d814e0
Kramer Jensen <Kramerjensend@gmail.com>
2021-05-01 17:16:18 -0700

    Created model and properties within the Product class

---
+++ LanguageFeatures/Models/Product.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LanguageFeatures.Models
+{
+    public class Product
+    {
+        public string Name { get; set; }
+
+        public decimal? Price { get; set; }
+
+        public static Product[] GetProducts()
+        {
+            Product kayak = new Product()
+            {
+                Name = "Kayak",
+                Price = 275m
+            };
+
+            Product lifeJacket = new Product()
+            {
+                Name = "Lifejacket",
+                Price = 48.95m
+            };
+
+            return new Product[] { kayak, lifeJacket, null };
+        }
+    }
+}
