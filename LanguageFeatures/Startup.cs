fb59783eb0c7aee514a1671c04d5e022cbe2c52e
Kramer Jensen <Kramerjensend@gmail.com>
2021-05-01 18:08:11 -0700

    Enabled the view

--- LanguageFeatures/Startup.cs
+++ LanguageFeatures/Startup.cs
@@ -9,16 +9,17 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.AspNetCore.Http;
-
+using Microsoft.AspNetCore.Mvc;
 
 namespace LanguageFeatures
 {
     public class Startup
     {
+
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddMvc(options => options.EnableEndpointRouting = false);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -28,12 +29,6 @@ namespace LanguageFeatures
             {
                 app.UseDeveloperExceptionPage();
             }
-
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
-
             app.UseMvcWithDefaultRoute();
         }
     }
